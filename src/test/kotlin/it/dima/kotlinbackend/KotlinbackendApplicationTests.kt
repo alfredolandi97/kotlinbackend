58df9a5a2eab65de53ed304193536110bb0938a7
alfredolandi97 <61738674+alfredolandi97@users.noreply.github.com>
2024-06-30 10:41:10 +0200

    First commit

---
+++ src/test/kotlin/it/dima/kotlinbackend/KotlinbackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.dima.kotlinbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinbackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
